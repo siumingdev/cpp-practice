a353cdd865a49e5870a1b401cd5ff2509216577d
Mike Chan <siumingdev@gmail.com>
2024-07-06 00:22:33 +0100

    Create q2.cpp

---
+++ discovering-modern-cpp-2ed/ch3/q2.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <sstream>
+
+template <typename T, typename ...P>
+void to_tuple_string_aux(std::ostringstream& oss, T const& x, P const& ...p) {
+    if constexpr (sizeof...(P)) {
+        oss << x << ',';
+        to_tuple_string_aux(oss, p...);
+    } else {
+        oss << x;
+    }
+}
+
+template <typename ...P>
+std::string to_tuple_string_recursive(P const& ...p) {
+    std::ostringstream oss;
+    oss << '(';
+    to_tuple_string_aux(oss, p...);
+    oss << ')';
+    return oss.str();
+}
+
+template <typename ...P>
+std::string to_tuple_string_recursive_lambda(P const& ...p) {
+    std::ostringstream oss;
+    oss << '(';
+
+    auto helper = [&oss](auto& self_ref, auto const& x, auto const& ...p_) {
+        if constexpr (sizeof...(p_)) {
+            oss << x << ',';
+            self_ref(self_ref, p_...);
+        } else {
+            oss << x;
+        }
+    };
+    
+    helper(helper, p...);
+    
+    oss << ')';
+    return oss.str();
+}
+
+int main() {
+    std::cout << to_tuple_string_recursive(1.1, "abc", "def") << std::endl;
+    std::cout << to_tuple_string_recursive_lambda(1.1, "abc", "def") << std::endl;
+    return 0;
+}
