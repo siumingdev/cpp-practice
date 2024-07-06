1909976def63207c3a21d34865d28f4fda305938
Mike Chan <siumingdev@gmail.com>
2024-07-06 19:46:02 +0100

    Update and rename q6.cpp to q6_7.cpp

--- discovering-modern-cpp-2ed/ch3/q6_7.cpp
+++ discovering-modern-cpp-2ed/ch3/q6_7.cpp
@@ -1,6 +1,7 @@
-#include <iostream>
 #include <cstddef>
+#include <iostream>
 #include <iterator>
+#include <ranges>
 
 class odd_iterator {
 public:
@@ -34,8 +35,24 @@ private:
     int value;
 };
 
+class odd_range {
+public:
+    odd_range(int min_, int max_) : min_iter{min_}, max_iter{max_} {}
+
+    odd_iterator begin() const { return min_iter; }
+    odd_iterator end() const { return max_iter; }
+
+private:
+    odd_iterator min_iter;
+    odd_iterator max_iter;
+};
+
 int main() {
+    static_assert(std::input_iterator<odd_iterator>);
     static_assert(std::forward_iterator<odd_iterator>);
+    static_assert(std::ranges::forward_range<odd_range>);
+
+    std::cout << "Test odd_iterator" << std::endl;
     
     odd_iterator i1, i2;
     std::cout << *i1 << std::endl;
@@ -45,5 +62,10 @@ int main() {
     std::cout << *i2 << std::endl;
     i1 = i2;
     std::cout << *i1 << std::endl;
+    
+    std::cout << "Test odd_range" << std::endl;
+
+    for (int i : odd_range(7, 27))
+        std::cout << i << std::endl;
     return 0;
 }
