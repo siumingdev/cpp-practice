49ee6c482d65300e58859de56c2a2a371d91e482
Mike Chan <siumingdev@gmail.com>
2024-07-06 14:01:39 +0100

    Update q2_fold.cpp

--- discovering-modern-cpp-2ed/ch3/q2_fold.cpp
+++ discovering-modern-cpp-2ed/ch3/q2_fold.cpp
@@ -5,7 +5,7 @@ template <typename T, typename ...P>
 std::string to_tuple_string(T const& t, P const& ...p) {
     std::ostringstream oss;
     oss << '(' << t;
-    ((oss << ',' << p), ...);
+    ((oss << ',' << p), ...);  // because `,` is a fold-exp supported binary op, free func signature: `T2& operator,(const T &a, T2 &b);`
     oss << ')';
     return oss.str();
 }
